0b6725579788b49c0610a3c811a181a1ffd794c9
GulnazNikitina <110427929+GulnazNikitina@users.noreply.github.com>
2023-01-14 09:51:49 -0700

    js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
 
-
+// trying to add reloud function, need to finish
 // function startOver() {
 //   level = 0;
 //   gamePattern = [];
